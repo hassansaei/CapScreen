adac0f0b9a53c1e289794a43cee933d008e48a87
Hassan Saei <94064114+hassansaei@users.noreply.github.com>
2025-07-04 11:07:06 +0200

    Bump version to 1.0.0-alpha.5

--- capscreen/version.py
+++ capscreen/version.py
@@ -1,3 +1,3 @@
 # capscreen/version.py
 
-__version__ = "1.0.0-alpha.4"
+__version__ = "1.0.0-alpha.5"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="capscreen",
-    version="1.0.0-alpha.4",
+    version="1.0.0-alpha.5",
     packages=find_packages(),
     package_data={
         "capscreen": [
